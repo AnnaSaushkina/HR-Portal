df06f7539610e7a4f509cf1c2676a3a84724037d
Anna Saushkina <saushkinanna@gmail.com>
2025-07-10 15:34:21 +0300

    init: Добавлено ТЗ и 1 версия карточек

---
+++ 1-sprint/script.js
@@ -0,0 +1,19 @@
+let btns = document.querySelectorAll('.card__button')
+
+let likeCounter = [...btns] // нод узлы сначала превращаем в массив
+
+let result = [] 
+likeCounter.forEach((btn) => {
+ result.push(btn)
+}) // каждый узел сначала должен быть превращен в массив
+
+let likeBtn = result[0]
+
+console.log(likeBtn)
+// добавление лайка
+likeBtn.addEventListener('click', () => {
+  likeBtn.classList.toggle('card__button-like')
+  console.log(likeBtn)
+})
+
+// console.log(check)
